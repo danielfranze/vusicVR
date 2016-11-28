88b08f67d4e1c0aec69136a0f6af064a930ed961
Daniel Franze <daniel.franze.office@gmail.com>
2016-11-28 17:58:46 +0100

    add distance between spheres

---
+++ Assets/Scripts/musicObject/createObject.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using createSphereIfPossible;
+
+public class createObject : MonoBehaviour
+{
+    private Valve.VR.EVRButtonId triggerButton;
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device controller;
+    private GameObject actorSphere;
+    private bool collision = false;
+
+    //private Collider col;
+    //private SphereCollider sphereCollider;
+
+    //Use this for initialization
+    void Start()
+    {
+
+        triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+      
+        //createSphereIsPossible = true;
+        //createSphereIfPossible.createObjectIfPossible.createSphereIsPossible = true;
+        //collision = false;
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        // Exception
+
+        controller = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (controller.GetPress(triggerButton) && createSphereIfPossible.createObjectIfPossible.createSphereIsPossible && !createSphereIfPossible.createObjectIfPossible.collision)
+        {
+            createSphereIfPossible.createObjectIfPossible.createSphereIsPossible = false;
+            createSphere();
+
+        }
+        //collision = false;
+    }
+
+    void createSphere()
+    {
+        actorSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        actorSphere.gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        actorSphere.transform.position = GameObject.FindGameObjectWithTag("ControllerSphere").transform.position;
+        actorSphere.GetComponent<Renderer>().material.color = Color.red;
+        actorSphere.AddComponent<Rigidbody>();
+        actorSphere.GetComponent<Rigidbody>().useGravity = false;
+        actorSphere.GetComponent<Rigidbody>().isKinematic = true;
+        actorSphere.tag = "Sound1";
+        actorSphere.GetComponent<SphereCollider>().radius = 0.65f;
+        actorSphere.GetComponent<SphereCollider>().isTrigger = true;
+        AudioSource audio = actorSphere.AddComponent<AudioSource>();
+        audio.clip = Resources.Load("HiHatOpen909") as AudioClip;
+    }
+
+
+}
---
+++ Assets/Scripts/musicObject/createObjectIfPossible.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace createSphereIfPossible
+{
+    public class createObjectIfPossible : MonoBehaviour
+    {
+        public static bool createSphereIsPossible;
+        public static bool collision;
+        // Use this for initialization
+        void Start()
+        {
+            createSphereIsPossible = true;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void OnTriggerExit()
+        {
+            createSphereIsPossible = true;
+            //collision = false;
+            /*
+            if (controller.GetPress(triggerButton))
+            {
+                createSphere();
+
+            }
+            */
+            collision = false;
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Sound1")
+            {
+                Debug.Log("Fuuuuck");
+                //collision = true;
+            }
+        }
+
+        void OnTriggerStay(Collider col)
+        {
+            collision = true;
+        }
+    }
+}
