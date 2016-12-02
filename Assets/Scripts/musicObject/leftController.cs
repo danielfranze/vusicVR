6971a131664595db67c346a2a438d51f3cf27d90
Daniel Franze <daniel.franze.office@gmail.com>
2016-12-02 21:54:47 +0100

    add instand playback & fast forward/rewind

---
+++ Assets/Scripts/musicObject/leftController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class leftController : MonoBehaviour
+{
+    private SteamVR_Controller.Device controller;
+    private Valve.VR.EVRButtonId touchPad;
+    private SteamVR_TrackedObject trackedObject;
+    private int fps = 0;
+
+    // Use this for initialization
+    void Start () {
+        touchPad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            if (movePointSensorNamespace.movePointSensor.speed >= 3f || movePointSensorNamespace.movePointSensor.speed <= -3f)
+            {
+                Debug.Log("speed nicht ok");
+                
+            }
+            if (controller.GetAxis(touchPad).y <= -0.5f)
+            {
+                movePointSensorNamespace.movePointSensor.speed -= 0.2f;
+            }
+
+            if (controller.GetAxis(touchPad).y >= 0.5f)
+            {
+                movePointSensorNamespace.movePointSensor.speed += 0.2f;
+            }
+
+            if (controller.GetAxis(touchPad).y < 0.5f && controller.GetAxis(touchPad).y > -0.5f)
+            {
+                movePointSensorNamespace.movePointSensor.speed = 0;
+            }
+
+
+
+        }
+    }
+}
--- Assets/Scripts/musicObject/movePointSensor.cs
+++ Assets/Scripts/musicObject/movePointSensor.cs
@@ -1,29 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movePointSensor : MonoBehaviour
+namespace movePointSensorNamespace
 {
-    public float speed;
-    Vector3 targetPos;
-    Vector3 startPos;
-    // Use this for initialization
-    void Start()
+    public class movePointSensor : MonoBehaviour
     {
-        speed = 1f;
-        targetPos = GameObject.FindGameObjectWithTag("Target").transform.position;
-        startPos = GameObject.FindGameObjectWithTag("Trigger").transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position == targetPos)
+        public static float speed;
+        Vector3 targetPos;
+        Vector3 start;
+        Vector3 spawn;
+        // Use this for initialization
+        void Start()
         {
-            transform.position = startPos;
+            speed = 0;
+            targetPos = GameObject.FindGameObjectWithTag("Target").transform.position;
+            start = GameObject.FindGameObjectWithTag("Start").transform.position;
+            spawn.x = 1.2f;
+            spawn.y = 0;
+            spawn.z = 0;
+
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            /*if (transform.position == targetPos)
+                {
+                    transform.position = start + spawn;
+                }
+                else
+                {
+                    Debug.Log("speed: " + speed);
+                    transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+                if (transform.position == start)
+                {
+                    Debug.Log("target1Pos !!!!!!!!!!!");
+                    transform.position = targetPos - spawn;
+                }
+            }*/
+
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Target")
+            {
+                Debug.Log("col target");
+                transform.position = start - spawn;
+            }
+            if (col.gameObject.tag == "Start")
+            {
+                Debug.Log("col start");
+                transform.position = targetPos + spawn;
+            }
         }
     }
 }
